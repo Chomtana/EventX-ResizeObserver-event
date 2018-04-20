532fd7a897741e7eac9c4c96aad417a3063d6249
Chomtana <Chomtana001@gmail.com>
2018-04-20 16:16:20 +0700

    First commit

---
+++ src/resize.js
@@ -0,0 +1,31 @@
+import evx from 'eventx-core';
+
+if (!window.evx) window.evx = evx;
+
+evx.setting.resizeObserverFirstRun = true;
+
+evx.createEvent("resize",function(target,callback) {
+    var firstrun = true;
+
+    const ro = new ResizeObserver(entries => {
+        for (let entry of entries) {
+            if (entry.target == target) {
+                if (evx.setting.resizeObserverFirstRun) {
+                    callback(entry);
+                } else {
+                    if (!firstrun) {
+                        callback(entry);
+                    }
+                }
+                firstrun = false;
+            }
+        }
+    });
+
+    ro.observe(target);
+
+    return function() {
+        //ro.unobserve(target);
+        ro.disconnect();
+    }
+});
